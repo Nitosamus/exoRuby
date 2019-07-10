235706879adff7b84628b8cc89b9b5e3a65d7305
Tonie <toniemontfort@gmail.com>
2019-07-10 11:40:03 +0300

    cinquième exo ruby

---
+++ exo_5.rb
@@ -0,0 +1,40 @@
+# annonciation du projet 
+puts "on ca compter le nombre d'heures de travail à THP"
+
+# on va dire le nombre d'heures qu'il faut chez THP
+puts "travail: #{10*5*11}"
+
+# si c'est en unité minute, comment ca se présente?
+puts "En minutes ça fait: #{10 * 5 * 11 * 60}"
+
+#si c'est en unité séconde, comment ca se présente?
+puts "Et en seconde?"
+puts 10 * 5 * 11* 60 * 60
+
+#on demande si 3+2 est inf à 5-7
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+# ici on va vérifier si c'est vrai ou faux
+puts 3 + 2 <  5 - 7
+
+# on demande ce que donne l'addition  3+2
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+
+#on demande ce que donne la soustraction  5 - 7
+puts "Ça fait combien 5 - 7 ? #{5 + 5}"
+
+ #voici notre conclusion
+puts "Ok, c'est faux alors !"
+
+#on commente le résultat
+puts "C'est drôle ca, faisons-en plus !"
+
+#on fait ici une comparaison avec l'opérateur >
+puts "Est-ce que 5 est plus grand que -2 ? #{5< -2}"
+
+# on fait ici une comparaison avec l'opérateur >=
+puts "Est-ce que 5 est supérieur ou égale à -2 ? #{5 >= -2}"
+
+# on fait ici une comparaison avec l'opérateur <=
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5<= -2}"
+
