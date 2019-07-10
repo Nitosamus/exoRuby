2368de781cef1689dea3a466d11542aaf3528d83
Tonie <toniemontfort@gmail.com>
2019-07-10 12:15:00 +0300

    les exo de exo 7 ruby

---
+++ exo_7b.rb
@@ -0,0 +1,5 @@
+#ici on met un petite introductionn et avant le champ vide on met > pour indiquer la supériorité
+puts "Bonjour, c'est quoi ton blase ?"
+print ">"
+user_name = gets.champ
+puts user_name
---
+++ exo_7c.rb
@@ -0,0 +1,3 @@
+#ici on laisse seulement un champ vide sans commentaire dessus
+user_name = gets.champ
+puts user name
